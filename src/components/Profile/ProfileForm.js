b0adba14e82ffad3e58affd88db325d1247eec34
FarhanKhan7749 <fk828633@gmail.com>
2023-03-16 12:04:49 +0530

    Updating the Old Password

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,11 +1,53 @@
+import { useContext, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
+import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const history = useHistory()
+  const authCtx = useContext(AuthContext);
+  const newPasswordInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    // add validation 
+
+    // send request 
+
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyD-MVZV20Q_pw4JIthVCDIfEkUOUYiGXoA',{
+      method: 'POST',
+      body:JSON.stringify({
+        idToken: authCtx.token,
+        password: enteredNewPassword,
+        returnSecureToken: true,
+      }),
+      headers:{
+        'Content-Type': 'application/json',
+      }
+    }).then(res => {
+      if(res.ok){
+        return res.json();
+      }else{
+        // show error module
+        let resError = 'Authentication fieled!';
+
+        throw new Error(resError); 
+      }
+    }).then((data)=>{
+      console.log(data);
+      history.replace('/');
+    }).catch((error)=>{
+      alert(error.message);
+    });
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' ref={newPasswordInputRef}/>
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
