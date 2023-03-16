86bb1c0fe2ad1fafb976e9e021d99e5b359313ff
FarhanKhan7749 <fk828633@gmail.com>
2023-03-16 15:33:53 +0530

    auto logout

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -3,25 +3,36 @@ import react, { useState } from "react";
 const AuthContext = react.createContext({
     token: "",
     isLoggedin: false,
-    login : (token) =>{},
-    logout: () => {}
+    login: (token) => { },
+    logout: () => { }
 });
 
 export const AuthContextProvider = (props) => {
-    const intialToken = localStorage.getItem('token')
+    const intialToken = localStorage.getItem('token');
     const [token, setToken] = useState(intialToken);
+    //
+    // setTimeout(()=>{
+    //     logouthandler();
+    // },5000);
+    //
 
     // const userIsLoggedIn = !!localStorage.getItem('token');
+    // let userIsLoggedIn = !!token;
+
     const userIsLoggedIn = !!token;
+
 
     const loginHandler = (token) => {
         setToken(token);
         localStorage.setItem('token', token);
+        setTimeout(()=>{
+            localStorage.removeItem('token');
+        },300000) // this will remove token after 5min.
     };
 
     const logouthandler = () => {
         setToken(null);
-        localStorage.removeItem('token')
+        localStorage.removeItem('token');
     };
 
     const contextValue = {
@@ -30,6 +41,11 @@ export const AuthContextProvider = (props) => {
         login: loginHandler,
         logout: logouthandler,
     };
+
+    // setTimeout(() => {
+    //     localStorage.removeItem('token');
+    //     userIsLoggedIn = false;
+    // }, 50000);
 
     return <AuthContext.Provider value={contextValue}>{props.children}</AuthContext.Provider>
 };
